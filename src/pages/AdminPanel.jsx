977f6f430024159d069c7afa53cecdd9db16426b
amliyanage <ashenmliyanage@gmail.com>
2024-09-15 14:23:03 +0530

    admin project card | footer developed

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -1,13 +1,14 @@
 import '../assets/css/admin/AdminPanel.css'
 import AdminNavBar from "./component/admin/AdminNavBar.jsx";
 import {SearchIcon} from "../util/Icons.jsx";
+import ProjectCard from "./component/admin/ProjectCard.jsx";
 
 const AdminPanel = () => {
   return (
     <div id={"adminPanel"} className={"d-flex justify-content-center"}>
 
         <div className={"w-75 mt-3"}>
-            <AdminNavBar />
+            <AdminNavBar/>
 
             <div className={"d-flex justify-content-between"}>
                 <div className="search d-flex align-items-center gap-3">
@@ -15,6 +16,17 @@ const AdminPanel = () => {
                     <input type="text" placeholder="Search.." className={"bg-transparent border-0"}/>
                 </div>
                 <button className={"border-0"}>New Project</button>
+            </div>
+
+            <div className="project-list mt-5">
+                <ProjectCard/>
+                <ProjectCard/>
+                <ProjectCard/>
+                <ProjectCard/>
+            </div>
+
+            <div className="footer mt-5 d-flex justify-content-center mb-5">
+                <h2>Copyright (c) 2024 - Design By Ashen</h2>
             </div>
         </div>
 
---
+++ src/pages/component/admin/ProjectCard.jsx
@@ -0,0 +1,20 @@
+import thumbnail from '../../../assets/image/thumbnail.png'
+import '../../../assets/css/admin/ProjectCard.css'
+
+const ProjectCard = ({ project }) => {
+    return(
+        <div className={"w-100"} id={"projectCard"}>
+            <div className="thumbnail mb-3">
+                <img className={"w-100"} src={thumbnail} alt=""/>
+            </div>
+            <div className={"infoHead d-flex align-items-center gap-3"}>
+            <h4>INTERNET</h4>
+                <div></div>
+                <h4>June 28, 2021</h4>
+            </div>
+            <h1 className={"mt-3"}>How to design a product that can grow itself 10x in year</h1>
+        </div>
+    )
+}
+
+export default ProjectCard;
